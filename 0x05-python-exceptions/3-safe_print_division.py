68206256dbbd8eb3fc727c7f9bbc06128d53a31c
chitaaaaa <kalkidan.tesfaye.tiruneh@gmail.com>
2022-06-20 14:14:27 -0700

    commit

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,15 @@
+#!/usr/bin/bash/python3
+def safe_print_division(a, b):
+    """ Divide two integers and print the result
+    """
+
+    try:
+        result = a / b
+    except ZeroDivisionError:
+        result = None
+    finally:
+        try:
+            print("Inside result: {}".format(result))
+        except NameError:
+            pass
+        return result
