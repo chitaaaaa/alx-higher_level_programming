375fea87c37c94bad779a7c3f8fa521603c76b48
chitaaaaa <kalkidan.tesfaye.tiruneh@gmail.com>
2022-06-20 14:44:42 -0700

    commit

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
-    """ print at most x integers from a list 
+    """ Print at most x integers from a list
     """
-    count = 0 
+    count = 0
     for index in range(x):
         try:
             print("{:d}".format(my_list[index]), end="")
         except (TypeError, ValueError):
             pass
         else:
-            cout += 1
+            count += 1
     print()
     return count
